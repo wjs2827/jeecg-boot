2c7a0ba5fe7fa19ab362b836d135eef33e63bf6a
wjs2827 <690402827@qq.com>
2019-11-23 14:38:28 +0800

    Spring-boot框架搭建

---
+++ fowl-center-impl/src/test/java/com/fowl/FowlCenterImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fowl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FowlCenterImplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ fowl-interface/src/test/java/com/fowl/fowlinterface/FowlInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fowl.fowlinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FowlInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
