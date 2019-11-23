92bef9b70c5f710c4037db8694dd7814c27adc31
jinshan.wang.it <jinshan.wang.it@baozun.com>
2019-11-23 12:39:51 +0800

    spring boot

---
+++ src/main/java/com/fowl/FowlV3Application.java
@@ -0,0 +1,13 @@
+package com.fowl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FowlV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FowlV3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/fowl/FowlV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fowl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FowlV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
