f9b0606a58c10ccfc10c62208ad88c4858bfcd98
wjs2827 <690402827@qq.com>
2019-11-24 21:50:35 +0800

    spring-boot

--- fowl-center-impl/src/main/java/com/fowl/start/AdminApplication.java
+++ fowl-center-impl/src/main/java/com/fowl/start/AdminApplication.java
@@ -2,11 +2,14 @@ package com.fowl.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value={"classpath:config/${spring.profiles.active}/application-${spring.profiles.active}.properties"},ignoreResourceNotFound=true,encoding="utf-8")
 public class AdminApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(AdminApplication.class, args);
     }
 
---
+++ fowl-interface/src/main/java/com/fowl/model/user/User.java
@@ -0,0 +1,4 @@
+package com.fowl.model.user;
+
+public class User {
+}
